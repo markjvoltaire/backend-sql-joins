af363ff901333ba73bc36cc45e699083fac90e05
Mark Voltaire <markjvoltaire@gmail.com>
2022-03-21 18:38:16 -0400

    test 4 passed

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -1,2 +1,20 @@
 -- find all customers first_name, last_name, address, and city
 -- Similar to exercise 3 but with customer, address, city
+-- find all the customers with the same adress and city
+
+
+SELECT 
+address, last_name, first_name, city
+FROM
+customer
+LEFT JOIN
+address
+ON
+customer.address_id = address.address_id
+LEFT JOIN 
+city
+ON
+city.city_id = address.city_id
+
+
+
